2f7a4ee6c876e8b6eb5a6f32323ba0fe745bd0ed
Danubia Macedo <dubamacedoo@gmail.com>
2023-11-05 15:08:12 -0400

    feat: interface data client

---
+++ src/app/cliente/cliente.ts
@@ -0,0 +1,13 @@
+
+export interface ClienteInfo{
+
+    id: number;
+    nome: string;
+    contato: string;
+    plano:string;
+    valor:number;
+    servidor:string;
+    vencido:boolean;
+    endereco:string;
+  
+  }
