950d5f2aa87249c072cd92423833c896b61c8cdc
Danubia Macedo <dubamacedoo@gmail.com>
2023-11-05 15:08:01 -0400

    feat: created module to client

---
+++ src/app/cliente/cliente.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListaClientesComponent } from './lista/lista.component';
+import {MatCardModule} from '@angular/material/card';
+import { ClienteService } from './cliente.service';
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
+import { ExibirClienteInfoComponent } from './exibir-cliente-info/exibir-cliente-info.component';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+
+
+@NgModule({
+  declarations: [
+    CadastroComponent,
+    ListaClientesComponent,
+    ExibirClienteInfoComponent
+  ],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTooltipModule
+  ],
+  exports:[ListaClientesComponent],
+  providers:[
+    ClienteService
+  ]
+})
+export class ClienteModule { }
