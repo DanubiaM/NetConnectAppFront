60b37bb5b3a61db64c9eff96276403bee6fc012e
Danubia Macedo <dubamacedoo@gmail.com>
2023-11-05 15:08:39 -0400

    feat: list client component

---
+++ src/app/cliente/lista/lista.component.ts
@@ -0,0 +1,51 @@
+import { ClienteService } from '../cliente.service';
+import { ClienteInfo } from '../cliente';
+import { Component, NgModule } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { ExibirClienteInfoComponent } from '../exibir-cliente-info/exibir-cliente-info.component';
+
+
+
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css'],
+})
+export class ListaClientesComponent {
+
+  titleSection = "Clientes Registrados"
+  clientes :ClienteInfo[];
+  clienteVencido = 'background-color: #bb4d4d; color: #fff; padding: 2px'
+  ativoCard = false; 
+  clienteService :ClienteService | undefined;
+
+
+
+  constructor(clienteService:ClienteService, public dialog: MatDialog){
+
+    this.clientes=clienteService.getCliente();
+    this.clienteService = clienteService;
+  }
+
+  onMouseOverOut(){
+    this.ativoCard = !this.ativoCard;
+  }
+
+  openDialog(_idClient: any){
+
+    const selectClient = this.clientes.filter((cliente)=> cliente.id == _idClient)
+
+
+    const dialogRef = this.dialog.open(ExibirClienteInfoComponent,{
+      data: selectClient[0],
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  
+  
+  }
+}
